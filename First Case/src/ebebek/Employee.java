78846cfd012d5485ea3691f1518ca70cebb50300
sevkiuysal <103686991+sevki09@users.noreply.github.com>
2022-10-19 15:36:38 +0300

    Employee.java

--- First Case/src/ebebek/Employee.java
+++ First Case/src/ebebek/Employee.java
@@ -1,7 +1,5 @@
 package ebebek;
 
-import java.text.DecimalFormat;
-
 public class Employee {
 	
 	private String name;
@@ -14,10 +12,6 @@ public class Employee {
 	
 	private int year=2021;
 	
-	DecimalFormat f = new DecimalFormat("##.00");
-	
-	private double rSalary;
-	
 	private int hours=40;
 
 	public Employee(String name, double salary, int workHours, int hireYear) {
@@ -27,38 +21,6 @@ public class Employee {
 		this.hireYear = hireYear;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-
-	public int getWorkHours() {
-		return workHours;
-	}
-
-	public void setWorkHours(int workHours) {
-		this.workHours = workHours;
-	}
-
-	public int getHireYear() {
-		return hireYear;
-	}
-
-	public void setHireYear(int hireYear) {
-		this.hireYear = hireYear;
-	}
-	
 	public double tax() {
 		double result=0;
 		if(this.salary>1000) {
@@ -81,33 +43,31 @@ public class Employee {
 		return extraWork;
 	}
 
-
 	public double raiseSalary() {
 		int raiseYear=year-this.hireYear;
 		double newSalary=0;
-		rSalary=this.salary+bonus()-tax();
 		if(raiseYear<10) {
-			newSalary=rSalary*0.05;
+			newSalary=this.salary*0.05;
 		}
 		else if(raiseYear>9 &&raiseYear<20) {
-			newSalary=rSalary*0.10;
+			newSalary=this.salary*0.10;
 		}
 		else if(raiseYear>19) {
-			newSalary=rSalary*0.15;
+			newSalary=this.salary*0.15;
 		}
-		return newSalary+rSalary-this.salary;
+		return newSalary;
 	}
 	
 	public String toString() {
-		String result="Adý :"+this.name+"\n"
-				+"Maaþ :"+this.salary+"\n"
-				+"Çalýþma Saati :"+this.workHours+"\n"
-				+"Baþlangýç Yýlý :"+this.hireYear+"\n"
-				+"Vergi :"+tax()+"\n"
-				+"Bonus :"+bonus()+"\n"
-				+"Maaþ Artýþý :"+f.format(raiseSalary())+"\n"
-			    +"Vergi ve Bonus ile Maaþ :"+f.format(rSalary)+"\n"
-				+"Toplam Maaþ :"+f.format((this.salary+raiseSalary()))+"\n";
+		String result="AdÄ± : "+this.name+"\n"
+				+"MaaÅŸ : "+this.salary+"\n"
+				+"Ã‡alÄ±ÅŸma Saati : "+this.workHours+"\n"
+				+"BaÅŸlangÄ±Ã§ YÄ±lÄ± : "+this.hireYear+"\n"
+				+"Vergi : "+tax()+"\n"
+				+"Bonus : "+bonus()+"\n"
+				+"MaaÅŸ ArtÄ±ÅŸÄ± : "+raiseSalary()+"\n"
+			    +"Vergi ve Bonuslar ile birlikte maaÅŸ : "+(this.salary+bonus()-tax())+"\n"
+				+"Toplam MaaÅŸ : "+(this.salary+raiseSalary())+"\n";
 		return result;
 		
 	}
